c780e9a02b3bf694364a7cdb80238b9215a7a207
Jay Vishaal J <jayvishaal17065@cse.ssn.edu.in>
2024-02-03 16:34:02 -0500

    added db as util and added example get

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+import DB
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.get('/hello')
 def hello():
+    db = DB.get_db()
+    try:
+        res = list(db.users.find({
+            "id" : "test"
+        }))
+        print(res)
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
     return 'Hello'
 
 if __name__ == '__main__':
---
+++ readme.md
@@ -0,0 +1,9 @@
+# To install the lib
+
+```
+
+python3 -m venv .venv
+source .venv/bin/activate
+python -m pip install "pymongo[srv]"
+
+```
